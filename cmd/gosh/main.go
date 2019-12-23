c7d9e7c8e17c2fbf3e3444841fe70bed8f9231d5
Christian Roese <christian.roese@mortonbuildings.com>
2019-12-23 10:52:39 -0600

    adds basic input reading

--- cmd/gosh/main.go
+++ cmd/gosh/main.go
@@ -1,11 +1,39 @@
 package main
 
 import (
-  "fmt"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 const defaultPrompt = "gosh>"
 
 func main() {
-  fmt.Printf("%s ", defaultPrompt)
+	fmt.Printf("%s ", defaultPrompt)
+	input, err := readUserInput()
+	if err != nil {
+		reportError(err)
+	}
+	fmt.Printf("gosh: got input: '%s'\n", input)
+}
+
+func reportError(err error) {
+	fmt.Fprintf(os.Stderr, "gosh: input error: %s\n", err)
+}
+
+func readUserInput() (string, error) {
+	stdin := bufio.NewScanner(os.Stdin)
+	input := ""
+	for stdin.Scan() {
+		line := stdin.Text()
+		if !strings.HasSuffix(line, "\\") {
+			input += line
+			break
+		} else {
+			input += strings.TrimSuffix(line, "\\")
+			fmt.Print("> ")
+		}
+	}
+	return input, stdin.Err()
 }
